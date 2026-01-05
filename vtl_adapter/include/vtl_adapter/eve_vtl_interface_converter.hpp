5ed4f9011814ef6594874ac709cba2bb33983ed1
Akihisa Nagata <54956813+asa-naki@users.noreply.github.com>
2026-01-05 15:14:03 +0900

    feat: update adapi (#64)
    * refactor: remove unused dependencies and subscriptions in EveVTLInterfaceConverter
    
    Signed-off-by: asa-naki <akihisa.nagata@tier4.jp>
    
    * style: improve conditional return formatting in EveVTLInterfaceConverter
    
    Signed-off-by: asa-naki <akihisa.nagata@tier4.jp>
    
    ---------
    
    Signed-off-by: asa-naki <akihisa.nagata@tier4.jp>

--- vtl_adapter/include/vtl_adapter/eve_vtl_interface_converter.hpp
+++ vtl_adapter/include/vtl_adapter/eve_vtl_interface_converter.hpp
@@ -17,14 +17,11 @@
 
 #include <memory>
 #include <unordered_map>
+
 #include "rclcpp/rclcpp.hpp"
 #include "tier4_v2x_msgs/msg/infrastructure_command.hpp"
 #include "tier4_v2x_msgs/msg/key_value.hpp"
-
-#include "autoware_adapi_v1_msgs/msg/route_state.hpp"
-#include "autoware_adapi_v1_msgs/msg/route.hpp"
 #include "autoware_adapi_v1_msgs/msg/operation_mode_state.hpp"
-#include "eve_cmd_gate_msgs/msg/engage_request_state.hpp"
 
 #include "vtl_adapter/eve_vtl_attribute.hpp"
 
@@ -33,12 +30,7 @@ namespace eve_vtl_interface_converter
 
 using EveVTLAttr = eve_vtl_attribute::EveVTLAttr;
 using InfrastructureCommand = tier4_v2x_msgs::msg::InfrastructureCommand;
-
-using RouteState = autoware_adapi_v1_msgs::msg::RouteState;
-using Route = autoware_adapi_v1_msgs::msg::Route;
 using OperationModeState = autoware_adapi_v1_msgs::msg::OperationModeState;
-using RouteData = autoware_adapi_v1_msgs::msg::RouteData;
-using AutonomousDrivingStartButton =eve_cmd_gate_msgs::msg::EngageRequestState;
 
 class EveVTLInterfaceConverter
 {
@@ -50,33 +42,23 @@ public:
   const InfrastructureCommand& command() const;
   std::optional<uint8_t> request() const;
   bool response(const uint8_t& response_bit) const;
+
 private:
   bool init(const InfrastructureCommand& input_command);
   std::string convertInfraCommand(const uint8_t& input_command) const;
   std::optional<std::string> convertADState() const;
 
-    // Subscription
-  rclcpp::Subscription<RouteState>::SharedPtr sub_routing_state_;
-  rclcpp::Subscription<Route>::SharedPtr sub_routing_route_;
+  // Subscription
   rclcpp::Subscription<OperationModeState>::SharedPtr sub_operation_mode_state_;
-  rclcpp::Subscription<AutonomousDrivingStartButton>::SharedPtr sub_autonomous_driving_start_button_;
 
   // Callback
-  void onState(const RouteState::ConstSharedPtr msg);
-  void onRoute(const Route::ConstSharedPtr msg);
   void onOperationModeState(const OperationModeState::ConstSharedPtr msg);
-  void onAutonomousDrivingStartButton(const AutonomousDrivingStartButton::ConstSharedPtr msg);
 
+  // Member variables
   InfrastructureCommand command_;
   std::shared_ptr<EveVTLAttr> vtl_attr_;
   rclcpp::Node* node_;
-  uint16_t state_;
-  Route route_;
-  bool is_autoware_control_enabled_;
-  bool is_in_transition_;
   uint8_t mode_;
-  bool is_accept_;
-  bool is_request_;
 };
 
 }  // namespace eve_vtl_interface_converter
